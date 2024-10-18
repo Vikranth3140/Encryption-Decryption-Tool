c905fae5fbd1150eb565953087a68616ca9ff517
anshu <anshug370@gmail.com>
2024-10-18 18:55:01 +0530

    Completed Fronted connect with script and all the functions. Some of the functions in script also changed throug this procss.

--- Frontend/main.py
+++ Frontend/main.py
@@ -19,33 +19,45 @@ heading_frame.pack(anchor=ctk.N, fill=ctk.X)
 heading_label = ctk.CTkLabel(heading_frame, text="Encryption and Decryption", font=("Helvetica", 30))
 heading_label.pack(anchor=ctk.CENTER, fill=ctk.X)
 
-key_frame = ctk.CTkFrame(app)
-key_frame.pack(anchor=ctk.N, fill=ctk.X)
-
-def Ebrowse():
+def Ebrowsekey():
 
     filename = ctk.filedialog.askopenfilename(filetypes=[("Encryption", "*.txt*"), ("Decryption", "*.decrypted*")])
     encryptfilename.insert(0, filename)
-def Dbrowse():
+def Dbrowsekey():
 
     filename = ctk.filedialog.askopenfilename(filetypes=[("Decryption", "*.encrypted*")])
     decryptfilename.insert(0, filename)
+def Ebrowsepass():
+
+    filename = ctk.filedialog.askopenfilename(filetypes=[("Encryption", "*.txt*"), ("Decryption", "*.decrypted*")])
+    encryptfilenamepass.insert(0, filename)
+def Dbrowsepass():
+
+    filename = ctk.filedialog.askopenfilename(filetypes=[("Decryption", "*.encrypted*")])
+    decryptfilenamepass.insert(0, filename)
 
 def EwithSecretKey():
     file = encryptfilename.get()
-    script.encrypt_file(file, 'key')
+    script.encrypt_file(file, 'key', 0)
 
 
 def DwithSecretKey():
-    file = encryptfilename.get()
-    script.decrypt_file(file, 'key')
+    file = decryptfilename.get()
+    script.decrypt_file(file, 'key', 0)
 
 
 def EwithPassword():
-    pass
+    file = encryptfilenamepass.get()
+    script.encrypt_file(file, 'password', 0)
 
 def DwithPassword():
-    pass
+    file = decryptfilenamepass.get()
+    script.decrypt_file(file, 'password', 0)
+
+# using Key
+
+key_frame = ctk.CTkFrame(app)
+key_frame.pack(anchor=ctk.N, fill=ctk.X)
 
 encryptfileLabel = ctk.CTkLabel(key_frame, text="Encrypt File", font=("Helvetica", 15))
 encryptfileLabel.pack(anchor=ctk.NW, padx=(20, 0), pady=(5, 0))
@@ -57,7 +69,7 @@ encrypt_frame.pack(anchor=ctk.NW, padx=20)
 encryptfilename = ctk.CTkEntry(encrypt_frame, width=450, font=("Helvetica", 15))
 encryptfilename.pack(side=ctk.LEFT)
 
-encryptbrowsebutton = ctk.CTkButton(encrypt_frame, text='Browse', command=Ebrowse)
+encryptbrowsebutton = ctk.CTkButton(encrypt_frame, text='Browse', command=Ebrowsekey)
 encryptbrowsebutton.pack(side=ctk.LEFT)
 
 encryptwithkey = ctk.CTkButton(key_frame, text='Encrypt', command=EwithSecretKey)
@@ -72,11 +84,48 @@ decrypt_frame.pack(anchor=ctk.NW, padx=20)
 decryptfilename = ctk.CTkEntry(decrypt_frame, width=450, font=("Helvetica", 15))
 decryptfilename.pack(side=ctk.LEFT)
 
-decryptbrowsebutton = ctk.CTkButton(decrypt_frame, text='Browse', command=Dbrowse)
+decryptbrowsebutton = ctk.CTkButton(decrypt_frame, text='Browse', command=Dbrowsekey)
 decryptbrowsebutton.pack(side=ctk.LEFT)
 
 decryptwithkey = ctk.CTkButton(key_frame, text='Decrypt', command=DwithSecretKey)
 decryptwithkey.pack(anchor=ctk.W, padx=20)
+
+# Using Password
+
+
+password_frame = ctk.CTkFrame(app)
+password_frame.pack(anchor=ctk.N, fill=ctk.X)
+
+encryptfileLabelpass = ctk.CTkLabel(password_frame, text="Encrypt File (Password)", font=("Helvetica", 15))
+encryptfileLabelpass.pack(anchor=ctk.NW, padx=(20, 0), pady=(5, 0))
+
+encrypt_framepass = ctk.CTkFrame(password_frame)
+encrypt_framepass.pack(anchor=ctk.NW, padx=20)
+
+# Add the entry and button to the new frame
+encryptfilenamepass = ctk.CTkEntry(encrypt_framepass, width=450, font=("Helvetica", 15))
+encryptfilenamepass.pack(side=ctk.LEFT)
+
+encryptbrowsebuttonpass = ctk.CTkButton(encrypt_framepass, text='Browse', command=Ebrowsepass)
+encryptbrowsebuttonpass.pack(side=ctk.LEFT)
+
+encryptwithkeypass = ctk.CTkButton(password_frame, text='Encrypt with Password', command=EwithPassword)
+encryptwithkeypass.pack(anchor=ctk.W, padx=20)
+
+decryptfileLabelpass = ctk.CTkLabel(password_frame, text="Decrypt File", font=("Helvetica", 15))
+decryptfileLabelpass.pack(anchor=ctk.NW, padx=(20,0), pady=(5,0))
+
+decrypt_framepass = ctk.CTkFrame(password_frame)
+decrypt_framepass.pack(anchor=ctk.NW, padx=20)
+
+decryptfilenamepass = ctk.CTkEntry(decrypt_framepass, width=450, font=("Helvetica", 15))
+decryptfilenamepass.pack(side=ctk.LEFT)
+
+decryptbrowsebuttonpass = ctk.CTkButton(decrypt_framepass, text='Browse', command=Dbrowsepass)
+decryptbrowsebuttonpass.pack(side=ctk.LEFT)
+
+decryptwithkeypass = ctk.CTkButton(password_frame, text='Decrypt with Password', command=DwithPassword)
+decryptwithkeypass.pack(anchor=ctk.W, padx=20)
 
 
 app.mainloop()
